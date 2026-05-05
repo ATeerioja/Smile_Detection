efb466025c1aca411d1ec3adfacb134bb45d4305
ATeerioja <teerioja.opiskelu@gmail.com>
2026-05-05 10:35:45 +0300

    Added Frown functionality

--- src/face_detection.py
+++ src/face_detection.py
@@ -54,27 +54,49 @@ while True:
         #Käytetään valmiita blendshapeja hymyn tunnistukseen
         smile_left = 0
         smile_right = 0
+        shrug_lower = 0
+        shrug_upper = 0
+        pucker = 0
+
 
         for b in blendshapes:
             if b.category_name == "mouthSmileLeft":
                 smile_left = b.score
             elif b.category_name == "mouthSmileRight":
                 smile_right = b.score
+            elif b.category_name == "mouthShrugUpper":
+                shrug_upper = b.score
+            elif b.category_name == "mouthShrugLower":
+                shrug_lower = b.score
+            elif b.category_name == "mouthPucker":
+                pucker = b.score
 
-        #Hymyarvon luonti ja testaaminen
+        #Hymyarvon ja mutruarvon luonti ja testaaminen
         smile_score = (smile_left + smile_right) / 2
+        frown_score = (shrug_upper + shrug_lower + pucker) / 3
 
-        if smile_score > 0.5:
-            text = f"Smiling ({smile_score:.2f})"
-            color = (0, 255, 0)
+        if smile_score > 0.3:
+            smile = f"Smiling ({smile_score:.2f})"
+            smile_color = (0, 255, 0)
         else:
-            text = f"Not Smiling ({smile_score:.2f})"
-            color = (0, 0, 255)
+            smile = f"Not Smiling ({smile_score:.2f})"
+            smile_color = (0, 0, 255)
+
+        if frown_score > 0.3:
+            frown = f"Frowning ({frown_score:.2f})"
+            frown_color = (0, 255, 0)
+        else:
+            frown = f"Not Frowning ({frown_score:.2f})"
+            frown_color = (0, 0, 255)
 
         #Näytetään arvauksen tulos videossa
-        cv2.putText(frame, text, (30, 50),
+        cv2.putText(frame, smile, (30, 50),
                     cv2.FONT_HERSHEY_SIMPLEX,
-                    1, color, 2, cv2.LINE_AA)
+                    1, smile_color, 2, cv2.LINE_AA)
+
+        cv2.putText(frame, frown, (30, 100),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1, frown_color, 2, cv2.LINE_AA)
 
     cv2.imshow("Smile Detection (MediaPipe Tasks)", frame)
 
