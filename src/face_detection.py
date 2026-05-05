4bfda5bd87b114110e749ca2f5112f5bd6709027
ATeerioja <teerioja.opiskelu@gmail.com>
2026-05-05 09:55:56 +0300

    Added comments to face_detection

--- src/face_detection.py
+++ src/face_detection.py
@@ -12,11 +12,13 @@ def result_callback(result, output_image, timestamp_ms):
 
 model_path = "../models/face_landmarker.task"
 
+#Tuodaan valmiit asetukset mallille
 BaseOptions = python.BaseOptions
 FaceLandmarker = vision.FaceLandmarker
 FaceLandmarkerOptions = vision.FaceLandmarkerOptions
 VisionRunningMode = vision.RunningMode
 
+#Asetetaan oletusasetukset
 options = FaceLandmarkerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.LIVE_STREAM,
@@ -24,27 +26,32 @@ options = FaceLandmarkerOptions(
     result_callback=result_callback
 )
 
+#Luodaan FaceLandmarker olio
 landmarker = FaceLandmarker.create_from_options(options)
 
+#Alustetaan live-video OpenCV kirjastolla
 cap = cv2.VideoCapture(0)
 timestamp = 0
 
 while True:
     ret, frame = cap.read()
-    if not ret:
-        break
 
+    #Luetaan yksi ruutu videosta
     frame = cv2.flip(frame, 1)
 
+    #Käsitellään video MediaPipen formaattiin
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
 
+    #Käytetään asynchronista metodia livevideon tunnistukseen
     landmarker.detect_async(mp_image, timestamp)
     timestamp += 1
 
+    #Käytetään kasvojen blendshapeja hymyn tunnistukseen
     if latest_result and latest_result.face_blendshapes:
         blendshapes = latest_result.face_blendshapes[0]
 
+        #Käytetään valmiita blendshapeja hymyn tunnistukseen
         smile_left = 0
         smile_right = 0
 
@@ -54,6 +61,7 @@ while True:
             elif b.category_name == "mouthSmileRight":
                 smile_right = b.score
 
+        #Hymyarvon luonti ja testaaminen
         smile_score = (smile_left + smile_right) / 2
 
         if smile_score > 0.5:
@@ -63,12 +71,14 @@ while True:
             text = f"Not Smiling ({smile_score:.2f})"
             color = (0, 0, 255)
 
+        #Näytetään arvauksen tulos videossa
         cv2.putText(frame, text, (30, 50),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     1, color, 2, cv2.LINE_AA)
 
     cv2.imshow("Smile Detection (MediaPipe Tasks)", frame)
 
+    #Painettaessa ESC suljetaan ikkuna
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
